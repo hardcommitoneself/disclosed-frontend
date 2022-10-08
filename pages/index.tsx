1eba619bce1e20dfe56f565f388290ae4b1bc511
hardcommitoneself <hardcommitoneself@gmail.com>
2022-10-07 19:51:45 -0700

    feat: simple usage of styled component and tailwind css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,17 @@
 import type { NextPage } from 'next'
+import styled from 'styled-components'
 
 const Home: NextPage = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Wrapper>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div.attrs({ className: 'flex flex-col' })`
+`
 
 export default Home
